063549d23c19d8c7469afe6bd4a44005f0274997
AuTa <auta520@live.com>
2022-12-07 17:54:52 +0800

    feat(project-gallery): add animation

--- src/app/project-gallery/project-gallery.component.ts
+++ src/app/project-gallery/project-gallery.component.ts
@@ -1,16 +1,24 @@
-import { Component, Input, OnInit, SimpleChanges } from '@angular/core'
+import { animate, state, style, transition, trigger } from '@angular/animations'
+import { Component, Input } from '@angular/core'
+
 import { Project } from '../project'
 
 @Component({
     selector: 'app-project-gallery',
     templateUrl: './project-gallery.component.html',
     styleUrls: ['./project-gallery.component.scss'],
+    animations: [
+        trigger('imgLoaded', [
+            state('false', style({ opacity: 0.5 })),
+            transition('false => true', [style({ opacity: 0.5 }), animate('500ms ease-in', style({ opacity: '*' }))]),
+        ]),
+    ],
 })
-export class ProjectGalleryComponent implements OnInit {
+export class ProjectGalleryComponent {
     @Input() project!: Project
     @Input() imagePath: string = ''
 
-    constructor() {}
+    loaded = false
 
-    ngOnInit(): void {}
+    constructor() {}
 }
