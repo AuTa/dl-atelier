62aaa5d44c6155e5edd8f4c4a3c509b8bbd43496
AuTa <auta520@live.com>
2022-12-10 12:07:47 +0800

    feat(class.project): support no main image & specify image pathes
    没有主图时显示黑色，不弹窗。
    可以指定特殊的图片路径。

--- src/app/project.ts
+++ src/app/project.ts
@@ -24,6 +24,8 @@ export class Project {
     mainImageIndex?: number
     beginImageIndex!: number
     endImageIndex!: number
+    mainImage?: string // 主图链接，如果存在则不考虑索引。
+    images?: string[] // 图片链接，如果存在则不考虑索引。
 
     getLangField(lang: LangStrings, field: FieldStrings): NgIterable<string> {
         const fieldMap: Record<FieldStrings, Record<LangStrings, () => string | string[]>> = {
@@ -38,6 +40,10 @@ export class Project {
         return f()
     }
 
+    get imagePathFolder(): string {
+        return this.imagePathPrefix ? `${this.imagePathPrefix}/` : ''
+    }
+
     /**
      * 通过索引获取图片地址
      * @param imageBasePath - 图片存储位置
@@ -45,21 +51,23 @@ export class Project {
      * @returns
      */
     imagePathByIndex(imageBasePath: string, imageIndex: number = 0): string {
-        return `${imageBasePath}/${this.imagePathPrefix}/${imageIndex
-            .toString()
-            .padStart(2, '0')}.jpg`
+        return `${imageBasePath}/${this.imagePathFolder}${imageIndex.toString().padStart(2, '0')}.jpg`
     }
 
     mainImagePath(imageBasePath: string): string {
+        if (this.mainImageIndex === undefined) {
+            return this.mainImage ? `${imageBasePath}/${this.imagePathFolder}${this.mainImage}` : ''
+        }
         return this.imagePathByIndex(imageBasePath, this.mainImageIndex)
     }
 
     imagePaths(imageBasePath: string): Array<string> {
+        if (this.images !== undefined && this.images.length > 0) {
+            return this.images.map(imagePath => `${imageBasePath}/${this.imagePathFolder}${imagePath}`)
+        }
         let imagePathes = []
         for (let i = this.beginImageIndex; i < this.endImageIndex; i++) {
-            let imagePath = `${imageBasePath}/${this.imagePathPrefix}/${i
-                .toString()
-                .padStart(2, '0')}.jpg`
+            const imagePath = this.imagePathByIndex(imageBasePath, i)
             imagePathes.push(imagePath)
         }
         return imagePathes
